43368f22079d63be651d440c372114568c68365f
yaojinyu <1043479087@qq.com>
2019-07-31 15:00:29 +0800

    创建国美app项目

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
